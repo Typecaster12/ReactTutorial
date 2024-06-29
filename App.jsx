58c24a6845a861df308dbb3cf6fe82b223ce590f
Harsh <168585844+Typecaster12@users.noreply.github.com>
2024-06-29 15:54:41 +0530

    Add files via upload
    This is tutorial for Components in react

---
+++ App.jsx
@@ -0,0 +1,38 @@
+//so components are the functions which returns JSX;
+//it is reuseable by declearing only once;
+//eg code
+
+//so this is a function which will call/return the components;
+//always use export before any thing which will finally show your output;
+export const Cards = () => {
+  return (
+    <div>
+      {/* // this is the method to call/use this components
+      //this compo can be call multiple times; */}
+      <TheComponent />
+      <TheComponent />
+      <TheComponent />
+      <TheComponent />
+    </div>
+  );
+};
+
+//the component function;
+//always write component in PascleCase;
+const TheComponent = () => {
+  return (
+
+    //as jsx returns only one element so always wrap the elements inside single parent element;
+    <div>
+
+      <div>
+        <img src="public/img.jpg" alt="" height="40%" width="40%" />
+      </div>
+
+      <h2>IronMan The Dead Hero!</h2>
+      <h3>Ratings: 9.9</h3>
+      <p>Reviews: Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit possimus nihil quo animi facilis quis dolore nisi nam placeat aut.</p>
+
+    </div>
+  );
+};
