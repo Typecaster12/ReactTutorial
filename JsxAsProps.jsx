1a52d2844588b041102d9ce87b6da26506bef809
Harsh <168585844+Typecaster12@users.noreply.github.com>
2024-07-09 13:38:26 +0530

    Add files via upload

---
+++ JsxAsProps.jsx
@@ -0,0 +1,62 @@
+//coding chlng how to pass jsx as props;
+export function Profile() {
+  return (
+    <div>
+      <p>Profile Cards Challenge</p>
+
+
+      {/* comp no1 */}
+      <ProfileCard
+        //props
+        name="Harsh"
+        age={21}
+        greeting={
+
+          //we can pass JSX inside props also like this;
+          <div>
+            <strong> Hi Harsh, Have a nice day!</strong> 
+          </div>
+        }
+      >
+
+        <p>Hobbies: Cricket, Reading</p>
+        <button>Contact</button>
+
+      </ProfileCard>
+
+      {/* comp no2 */}
+      <ProfileCard
+        //props
+        name="Molu"
+        age={22}
+        greeting={
+          <div>
+            <strong> Hi Molu, Have a nice day!</strong>
+          </div>
+        }
+      >
+
+        <p>Hobbies: Cricket, Reading</p>
+        <button>Contact</button>
+
+      </ProfileCard>
+
+    </div>
+  );
+}
+
+//To show the components first make ProfileCards function;
+function ProfileCard(props) {
+  const { name, age, greeting } = props;
+
+  return (
+    <>
+      <h1>The Name is:{name}</h1>
+      <h3>The age is:{age}</h3>
+      <h4>{greeting}</h4>
+      <p>{props.children}</p>
+    </>
+  );
+}
+
+// so if there is nested elements inside the child component we can access them using .children keyword
